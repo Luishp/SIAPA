531314cd4d683874684d46872dcc9a90938b393f
joao <joao.hfunes@gmail.com>
2014-11-20 23:32:52 -0600

    punto d venta

---
+++ src/main/java/com/siapa/managedbean/PuntodeVentaManagedBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siapa.managedbean;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Joao
+ */
+
+@Named("puntodeVentaManagedBean")
+@ViewScoped
+public class PuntodeVentaManagedBean implements Serializable {
+    
+}
